ac9255fe819e90724675ca767404ea8d8a40ecb8
Bryan Jeff Saycon <bryanjeff.saycon@g.msuiit.edu.ph>
2022-04-14 21:05:23 +0800

    added dotenv package dependency

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 // Simple Authentication System Sign-in/Sign-up with express js 
 //TODO : Connect Database to MongoDB
-
+require('dotenv').config();
 const express = require("express"); 
 const app = express();
 const cors = require('cors');
+const mongoose = require('mongoose');
+const PORT = process.env.PORT || 8080;
 
 // Cross Origin Resource Sharing
 app.use(cors());
@@ -20,8 +22,9 @@ app.use('/register', require('./routes/register'));
 // post request handler for login
 app.use('/login', require('./routes/login'));
 
-const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+// mongodb+srv://bejsaycon:<password>@cluster0.oezuo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
