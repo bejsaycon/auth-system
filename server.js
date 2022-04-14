ab7b8aa617e2a858f12422cb4c18f60b889a2737
Bryan Jeff Saycon <bryanjeff.saycon@g.msuiit.edu.ph>
2022-04-14 21:30:43 +0800

    connected to mongoDB

--- server.js
+++ server.js
@@ -1,30 +1,33 @@
-// Simple Authentication System Sign-in/Sign-up with express js 
+// Simple Authentication System Sign-in/Sign-up with express js
 //TODO : Connect Database to MongoDB
-require('dotenv').config();
-const express = require("express"); 
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
+const cors = require("cors");
+const mongoose = require("mongoose");
+const connectDB = require("./config/dbConn");
 const PORT = process.env.PORT || 8080;
+
+//Connect to MongoDB
+connectDB();
 
 // Cross Origin Resource Sharing
 app.use(cors());
 
-// built in middleware for json 
+// built in middleware for json
 app.use(express.json());
 
 //api for users data
-app.use('/users', require('./routes/api/users'));
+app.use("/users", require("./routes/api/users"));
 
 // post request handler for register user
-app.use('/register', require('./routes/register'));
+app.use("/register", require("./routes/register"));
 
 // post request handler for login
-app.use('/login', require('./routes/login'));
+app.use("/login", require("./routes/login"));
 
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
+  app.listen(PORT, () => {console.log(`Server running on port ${PORT}`)});
 });
 
-// mongodb+srv://bejsaycon:<password>@cluster0.oezuo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
