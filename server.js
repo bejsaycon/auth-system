ead9a79512b0a3f3f217e62c49717b19e5bbef17
Bryan Jeff Saycon <bryanjeff.saycon@g.msuiit.edu.ph>
2022-04-17 12:43:57 +0800

    Added logger middleware

--- server.js
+++ server.js
@@ -4,12 +4,17 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
 const mongoose = require("mongoose");
 const connectDB = require("./config/dbConn");
 const PORT = process.env.PORT || 8080;
 
 //Connect to MongoDB
 connectDB();
+
+// custom middleware logger
+app.use(logger);
 
 // Cross Origin Resource Sharing
 app.use(cors());
@@ -25,6 +30,8 @@ app.use("/register", require("./routes/register"));
 
 // post request handler for login
 app.use("/login", require("./routes/login"));
+
+app.use(errorHandler);
 
 mongoose.connection.once("open", () => {
   console.log("Connected to MongoDB");
