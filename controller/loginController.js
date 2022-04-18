038910569f29c986a91ff6e96e2d731c0615f34e
Bryan Jeff Saycon <bryanjeff.saycon@g.msuiit.edu.ph>
2022-04-18 22:02:15 +0800

    filter boolean

--- controller/loginController.js
+++ controller/loginController.js
@@ -11,7 +11,7 @@ const handleLogin = async (req, res) => {
     //check password
     const match = await bcrypt.compare(pwd, foundUser.password);
     if (match) {
-        const roles = Object.values(foundUser.roles);
+        const roles = Object.values(foundUser.roles).filter(Boolean);
         const accessToken = jwt.sign(
             {
                 "UserInfo": {
