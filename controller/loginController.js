5392f9726d5da366bf0a1d87048b3f69dd937705
Bryan Jeff Saycon <bryanjeff.saycon@g.msuiit.edu.ph>
2022-04-19 23:07:29 +0800

    added roles to pass on when logged in

--- controller/loginController.js
+++ controller/loginController.js
@@ -31,7 +31,7 @@ const handleLogin = async (req, res) => {
         const result = await foundUser.save();
         console.log(result);
         res.cookie('jwt', refreshToken, { httpOnly: true, sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 }); //secure: true, 
-        res.json({ accessToken });
+        res.json({ accessToken, roles });
     }
     else {
         res.sendStatus(401);
